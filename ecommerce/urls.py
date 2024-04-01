b142491296d9ddca516576c84ad88e9bcdae0d05
RashmiLnu <Rashmi.Lnu@unh.edu>
2024-03-31 21:04:57 -0400

    Basic setup for the SignUp, login, logout

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,4 +22,5 @@ from django.urls import include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("ecommerce_app.urls")),
+    path("auth/", include("authCart.urls")),
 ]
