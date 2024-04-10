79b4ee0a48759047611f4c6efda87551caa98eb5
RashmiLnu <Rashmi.Lnu@unh.edu>
2024-04-10 08:33:55 -0400

    Create model and UI design for Contact_Us page

--- ecommerce_app/views.py
+++ ecommerce_app/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from ecommerce_app.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("pnumbers")
+        message = request.POST.get("message")
+        myquesry = Contact(name=name, email=email, phone=phone, message=message)
+        myquesry.save()
+        messages.info(request, "Thank you for contacting us. We will get back to you soon.")
+        return render(request, "contact.html")
+
     return render(request, "contact.html")
 
 def about(request):
